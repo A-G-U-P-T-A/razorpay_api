1d136a163e24490206089335bcc37d7272ea1e90
Arnab Gupta <arnab@engineeringbaba.com>
2020-10-28 22:56:09 +0530

    new changes

--- src/main/java/com/example/razorpay/RazorpayApplication.java
+++ src/main/java/com/example/razorpay/RazorpayApplication.java
@@ -1,9 +1,5 @@
 package com.example.razorpay;
 
-import com.razorpay.Order;
-import com.razorpay.RazorpayClient;
-import com.razorpay.RazorpayException;
-import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
